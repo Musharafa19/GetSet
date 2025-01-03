d048a9f455c3bcd18cb36991f0c941943452bafe
Abena Rapha <mushabena18@gmail.com>
2025-01-03 00:42:22 +0000

    first commit

---
+++ Student.java
@@ -0,0 +1,15 @@
+public class Student {
+    
+        String name = Abena Rapha;
+        int Total score = 80;
+    
+    
+        Public getName(){
+            return  }
+    
+        Public getScore(){
+                return totalScore}
+
+    }
+    
+}
